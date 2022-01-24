01eda8db497251f030b92148c8c36ae7cd9f946c
snss231 <e0543670@u.nus.edu>
2022-01-25 00:12:38 +0800

    heroku commit

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_01_14_084957) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "description"
